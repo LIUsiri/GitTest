2a2ea5bd1b5a64aa154ca9c497b8f919a12d64f7
liusir <18503041143@163.com>
2018-01-19 16:09:03 +0800

    "新增"

---
+++ src/com/springmvc/solr/controller/TestGit.java
@@ -0,0 +1,18 @@
+package com.springmvc.solr.controller;
+/**
+ * class_name: TestGit
+ * package: com.springmvc.solr.controller
+ * describe: TODO
+ * @author: Liuxianglong
+ * @date: 2018/1/19
+ * creat_time: 15:56
+ **/
+
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("这是用来测试git");
+    }
+}
